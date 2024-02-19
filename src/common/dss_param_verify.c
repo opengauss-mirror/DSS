c9b32783c26d49f75c1c51273ba08ed7a1111cbe
chenzhikai <895543892@qq.com>
2024-02-19 16:29:24 +0800

    调整cluster run mode修改方式

--- src/common/dss_param_verify.c
+++ src/common/dss_param_verify.c
@@ -345,6 +345,34 @@ status_t dss_notify_audit_level(void *se, void *item, char *value)
     return CM_SUCCESS;
 }
 
+status_t dss_verify_cluster_run_mode(void *lex, void *def)
+{
+    char *value = (char *)lex;
+    if (cm_strcmpi(value, "cluster_standby") != 0 && cm_strcmpi(value, "cluster_primary") != 0) {
+        DSS_THROW_ERROR(ERR_DSS_INVALID_PARAM, "CLUSTER_RUN_MODE");
+        return CM_ERROR;
+    }
+    
+    int32 iret_snprintf =
+        snprintf_s(((dss_def_t *)def)->value, CM_PARAM_BUFFER_SIZE, CM_PARAM_BUFFER_SIZE - 1, "%s", value);
+    DSS_SECUREC_SS_RETURN_IF_ERROR(iret_snprintf, CM_ERROR);
+    return CM_SUCCESS;
+}
+
+status_t dss_notify_cluster_run_mode(void *se, void *item, char *value)
+{
+    if (cm_strcmpi(value, "cluster_standby") == 0) {
+        g_inst_cfg->params.cluster_run_mode = CLUSTER_STANDBY;
+        LOG_RUN_INF("The cluster_run_mode is cluster_standby.");
+    } else if (cm_strcmpi(value, "cluster_primary") == 0) {
+        g_inst_cfg->params.cluster_run_mode = CLUSTER_PRIMARY;
+        LOG_RUN_INF("The cluster_run_mode is cluster_primary.");
+    } else {
+        DSS_RETURN_IFERR2(CM_ERROR, DSS_THROW_ERROR(ERR_DSS_INVALID_PARAM, "failed to load params, invalid CLUSTER_RUN_MODE"));
+    }
+    return CM_SUCCESS;
+}
+
 #ifdef __cplusplus
 }
 #endif
